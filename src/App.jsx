ef4dd562001ff3fcbd96873d30b2ad95402696a0
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-23 17:00:01 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center w-full h-screen bg-gray-900">
-        <h1 className="text-3xl font-bold text-center text-blue-400">
-          Vite + React + Tailwind CSS
-        </h1>
-        <p>Hello World</p>
-      </div>
+      <h1>Hello!</h1>
     </>
   );
 }
